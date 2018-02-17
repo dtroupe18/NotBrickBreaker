9af1933beb5f28c3de4dead8a8e008c8736a355c
Dave <Dave.Troupe@gmail.com>
2018-02-17 17:46:45 -0500

    Initial Commit

--- NotBrickBreaker/GameScene.swift
+++ NotBrickBreaker/GameScene.swift
@@ -7,104 +7,63 @@
 //
 
 import SpriteKit
-import GameplayKit
+import AVFoundation
 
 class GameScene: SKScene {
     
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
+    let ballCategoryName = "yellowBall"
+    let paddleCategoryName = "paddle"
+    let brickCategoryName = "brick"
     
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var musicPlayer = AVAudioPlayer()
     
-    override func sceneDidLoad() {
-
-        self.lastUpdateTime = 0
+    override init(size: CGSize) {
+        super.init(size: size)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        if let backgroundMusicURL = Bundle.main.url(forResource: "BackgroundMusic", withExtension: "wav") {
+            do {
+              try musicPlayer = AVAudioPlayer(contentsOf: backgroundMusicURL)
+                musicPlayer.numberOfLoops = -1
+                musicPlayer.prepareToPlay()
+                musicPlayer.play()
+            }
+            catch {
+                print("Error Playing Music")
+            }
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        let bgImage = SKSpriteNode(imageNamed: "Background")
+        bgImage.position = CGPoint(x: self.frame.width / 2, y: self.frame.height / 2)
+        self.addChild(bgImage)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        self.physicsWorld.gravity = CGVector(dx: 0.0, dy: 0.0)
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        let worldBorder = SKPhysicsBody(edgeLoopFrom: self.frame)
+        self.physicsBody = worldBorder
+        self.physicsBody?.friction = 0
+        
+        let ball = SKSpriteNode(imageNamed: "Ball")
+        ball.size = CGSize(width: 25, height: 25)
+        ball.name = ballCategoryName
+        ball.position = CGPoint(x: self.frame.size.width / 4, y: self.frame.size.height / 4)
+        self.addChild(ball)
+        
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.frame.width / 2) // (diameter / 2) = radius
+        ball.physicsBody?.friction = 0
+        ball.physicsBody?.allowsRotation = false
+        ball.physicsBody?.linearDamping = 0
+        ball.physicsBody?.restitution = 1
+        ball.physicsBody?.applyImpulse(CGVector(dx: 10, dy: -10))
+        
+        let paddle = SKSpriteNode(imageNamed: "Paddle")
+        paddle.name = paddleCategoryName
+        paddle.position = CGPoint(x: self.frame.midX, y: paddle.frame.height * 2)
+        
+        self.addChild(paddle)
+        
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-        
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
-        }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
     }
 }
--- NotBrickBreaker/GameViewController.swift
+++ NotBrickBreaker/GameViewController.swift
@@ -14,30 +14,43 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+//
+//        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
+//        // including entities and graphs.
+//        if let scene = GKScene(fileNamed: "GameScene") {
+//
+//            // Get the SKScene from the loaded GKScene
+//            //
+//            if let sceneNode = scene.rootNode as! GameScene? {
+//
+//                // Set the scale mode to scale to fit the window
+//                //
+//                sceneNode.scaleMode = .aspectFill
+//
+//                // Present the scene
+//                if let view = self.view as! SKView? {
+//                    view.presentScene(sceneNode)
+//                    view.ignoresSiblingOrder = true
+//                    view.showsFPS = true
+//                    view.showsNodeCount = true
+//                }
+//            }
+//        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
         
-        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
-        // including entities and graphs.
-        if let scene = GKScene(fileNamed: "GameScene") {
-            
-            // Get the SKScene from the loaded GKScene
-            if let sceneNode = scene.rootNode as! GameScene? {
+        if let skView = self.view as! SKView? {
+            if skView.scene == nil {
                 
-                // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
+                skView.showsFPS = true
+                skView.showsNodeCount = true
                 
-                // Set the scale mode to scale to fit the window
-                sceneNode.scaleMode = .aspectFill
+                let gameScene = GameScene(size: skView.bounds.size)
+                gameScene.scaleMode = .aspectFill
                 
-                // Present the scene
-                if let view = self.view as! SKView? {
-                    view.presentScene(sceneNode)
-                    
-                    view.ignoresSiblingOrder = true
-                    
-                    view.showsFPS = true
-                    view.showsNodeCount = true
-                }
+                skView.presentScene(gameScene)
             }
         }
     }
